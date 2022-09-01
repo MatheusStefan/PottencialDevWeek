9076d177e448b1a686cf3ef65df3730a1f86e96b
MatheusStefan <roccomatheus0@gmail.com>
2022-09-01 15:42:48 -0300

    Criação de Modelo/Entidade quando acessado ao banco de dados

--- src/Persistence/DatabaseContext.cs
+++ src/Persistence/DatabaseContext.cs
@@ -4,7 +4,30 @@ using src.Models;
 namespace src.Persistence;
 
 public class DatabaseContext: DbContext{
+
+    //passando de um construtor padrão para um construtor do Entity Framework
+    public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options){
+        
+    }
     
+    //modelos que estão dentro do contexto do banco de dados
     public DbSet<Person> Persons { get; set; }
     public DbSet<Contract> Contracts { get; set; }
+
+    //criação dos modelos
+    protected override void OnModelCreating(ModelBuilder builder) {
+       //construção de entidade Pessoa com as seguintes instruções: 
+        builder.Entity<Person>(e =>{
+            
+            e.HasKey( e => e.Id); //uma pessoa tem uma chave
+            e
+            .HasMany(e => e.contratos) //uma pessoa tem muitos contratos.
+            .WithOne()                  //pra cada um
+            .HasForeignKey(e => e.PersonId);// tem uma chave estrangeira
+        });
+        builder.Entity<Contract>(e =>{
+            e.HasKey( e => e.Id);
+        });
+    }
+
 }
