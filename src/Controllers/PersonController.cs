1166801d0e22b066fb8ed60ec4e0c10857ac1eee
MatheusStefan <roccomatheus0@gmail.com>
2022-09-02 13:58:20 -0300

    Refatoração de HTTP Requests e ActionResults

--- src/Controllers/PersonController.cs
+++ src/Controllers/PersonController.cs
@@ -14,57 +14,102 @@ namespace src.Controllers;
                         //resultando nesse caso a rota "/person"
 
 //os dois pontos significa herança de uma classe                        
-public class PersonController: ControllerBase {
+public class PersonController : ControllerBase
+{
 
     //propriedade para que essa classe encontre o banco de dados e faça chamadas
     private DatabaseContext _repository { get; set; }
 
-    public PersonController(DatabaseContext repository){
+    public PersonController(DatabaseContext repository)
+    {
         this._repository = repository;
     }
 
-//criação de metodo get que através da rota "/person", retorna uma pessoa
+    //criação de metodo get que através da rota "/person", retorna uma pessoa
     [HttpGet]
-    public ActionResult<List<Person>> GetPerson(){
-        
+    public ActionResult<List<Person>> GetPerson()
+    {
+
         var result = _repository.Persons.Include(p => p.contratos).ToList();
-        if(!result.Any()) {
+        if (!result.Any())
+        {
             return NoContent();
-        } 
+        }
         return Ok(result);
     }
-//criação de metodo post
+    //criação de metodo post
     [HttpPost]
-    public Person PostPerson([FromBody]Person pessoa) {
-        _repository.Persons.Add(pessoa);
-        _repository.SaveChanges();
-        return pessoa;
-    }
+    public ActionResult<Person> PostPerson([FromBody] Person pessoa)
+    {
 
-//criação de metodo put
-    [HttpPut("{id}")]
-    public string UpdatePerson([FromRoute]int id, [FromBody]Person pessoa) {
-        _repository.Persons.Update(pessoa);
-        _repository.SaveChanges();
-        return "Dados do id " + id + "atualizados";
-    }
-
-//criação de metodo delete
-    [HttpDelete("{id}")]
-    public ActionResult<Object> DeletePersonById([FromRoute]int id) {
-        var result = _repository.Persons.SingleOrDefault(e => e.Id == id);
-
-        if(result is null) {
+        try
+        {
+            _repository.Persons.Add(pessoa);
+            _repository.SaveChanges();
+            
+        }
+        catch (System.Exception)
+        {
+            
             return BadRequest(new {
-                msg = "Solicitação inválida.",
+                msg = "Não foi possível realizar o cadastro.",
                 status = 400
             });
         }
-        _repository.Persons.Remove(result);
-        _repository.SaveChanges();
-        return Ok(new {
-            msg = "deletando pessoa de Id " + id,
-            status = 200
-        });
+        return Created("Cadastro feito com sucesso,  " , pessoa);
     }
-}
+
+    //criação de metodo put
+    [HttpPut("{id}")]
+    public ActionResult<Object> UpdatePerson([FromRoute] int id, [FromBody] Person pessoa)
+    {
+        var result = _repository.Persons.SingleOrDefault(e => e.Id == id);
+         if(result is null) {
+                return NotFound(new {
+                    msg = "registro não encontrado!",
+                    status = 404
+                });
+            }
+        try
+        {
+            _repository.Persons.Update(pessoa);
+            _repository.SaveChanges();
+        }
+        catch (System.Exception)
+        {
+
+            return BadRequest(new{
+                msg = "Algo deu errado!",
+                status = 400
+            });
+        }
+
+            return Ok(new
+            {
+                msg = "Dados do id " + id + " atualizados!",
+            });
+        }
+
+        //criação de metodo delete
+        [HttpDelete("{id}")]
+        public ActionResult<Object> DeletePersonById([FromRoute] int id)
+        {
+            var result = _repository.Persons.SingleOrDefault(e => e.Id == id);
+
+            if (result is null)
+            {
+                return BadRequest(new
+                {
+                    msg = "Solicitação inválida.",
+                    status = 400
+                });
+            }
+            _repository.Persons.Remove(result);
+            _repository.SaveChanges();
+            return Ok(new
+            {
+                msg = "deletando pessoa de Id " + id,
+                status = 200
+            });
+        }
+    }
